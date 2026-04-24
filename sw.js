6b901599186af13745b3f9ac91d8e9ce8083ea53
michaelluedecke1-cell <michael.luedecke1@googlemail.com>
2026-04-24 14:15:39 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'gourmet-archiv-v1';
+const ASSETS = [
+  'index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+// Installation: Dateien cachen
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivierung: Alte Caches löschen
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Netzwerk-Anfragen abfangen
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
